ecb3da90c4a5794f115fff64999fb1e30a7729e2
camilo muñoz <ingcamilo2022@gmail.com>
2022-05-06 19:54:36 -0500

    ciclos

---
+++ for.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 18:35:18 2022
+
+@author: Cami89
+"""
+
+#numero primo
+
+# primeMumber=int(input("enter a positive number"))
+# cont=0
+
+# if primeMumber>0:
+#     for i in range(1,primeMumber):
+        
+#         if primeMumber%i==0:
+#             cont+=1
+#     if cont==1:
+#         print(f"{primeMumber} es un numero primo")
+#     else:
+#         print(f"{primeMumber} no es un numero primo")
+# else:
+#     print(f"{primeMumber} no es un numero positivo")
+
+#menor,mayor y media
+
+# number=int(input("ingrese la cantidad de numeros a evaluar"))
+
+# acu=0
+# menor=0
+# mayor=0
+# test=0
+# for i in range(0,number):
+        
+#     number1=int(input(f"ingrese el numero {i+1  }"))
+#     acu+=number1
+    
+#     if i==0:
+#         mayor=number1
+#         menor=number1
+# #se requiere que el segundo digito del for sea mayor que el primero para ordenar el mayor
+#     if mayor<number1:
+#         mayor=number1
+#     if menor>number1:
+#         menor=number1
+    
+# print(f"la media es {acu/number}")
+
+# print(f"el numero mayor es  {mayor}")
+# print(f"el numero menor es  {menor}")
+
+#factorial
+# fac=int(input("ingrese el numero para obtener su factorial"))
+# acu=1
+# for i in range(1,fac+1):
+#     acu=acu*(i)
+# print(f"factorial de fac es : {acu}")
+
+
